cc825e66c4ab3d9d4575bd32ec32a473d9d5d0e0
Salas <salas@adm.usbcali.edu.co>
2024-11-20 16:03:03 -0500

    conexion db

--- proyecto/routes/index.js
+++ proyecto/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const driver = require('../db')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',async function(req, res, next) {
+
+  const session = driver.session()
+
+  try {
+    
+    const result = await session.run('MATCH (n) RETURN n LIMIT 10')
+
+    result.records.forEach(record => {
+      console.log(record)
+    })
+
+  } catch (error) {
+    
+  }finally {
+    await session.close()
+  }
+
   res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
