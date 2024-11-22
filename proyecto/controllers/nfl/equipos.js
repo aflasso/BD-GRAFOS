0ac37ea491e19bdc79a6a26580992601812d24a4
aflasso <andresfelipe.lasso@gmail.com>
2024-11-22 18:30:54 -0500

    post, get nfl equipos

--- proyecto/controllers/nfl/equipos.js
+++ proyecto/controllers/nfl/equipos.js
@@ -1,1 +1,38 @@
-const {} = require('../../models/jugador')
+const {DeportistaNFL} = require('../../models/jugador')
+const {response, request} = require('express')
+
+const getAll = async (req = request, res = response) => {
+
+    try {
+        const deportista = await DeportistaNFL.all({deporte_asignado: 'nfl'})
+        
+        deportista.forEach(element => {
+            console.log(element.get('nombre'));
+            
+        })
+
+        res.render('index', { title: `NFL EQUIPOS GET ${deportista}` });
+    } catch (error) {
+        console.error(error);
+        
+    }
+    
+}
+
+const post = async (req, res, next) => {
+
+    console.log('entre al post')
+    const deportista = req.body
+
+    try {
+        
+        const jugadorNuevo = await DeportistaNFL.create(deportista)
+
+    } catch (error) {
+        console.error(error);
+        
+    }
+    res.render('index', { title: `NFL EQUIPOS POST ${deportista}` });
+}
+
+module.exports = {getAll, post}
